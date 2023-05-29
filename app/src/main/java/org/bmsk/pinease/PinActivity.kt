18f1674b69525f5adc84231dbf7c4ebe986bce54
bmsk <vv99911@gmail.com>
2023-05-29 19:35:42 +0900

    Feat: Keypad 연동

--- app/src/main/java/org/bmsk/pinease/PinActivity.kt
+++ app/src/main/java/org/bmsk/pinease/PinActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import org.bmsk.pinease.databinding.ActivityPinBinding
+import org.bmsk.pinease.widget.ShuffleNumberKeyboard
 
-class PinActivity: AppCompatActivity() {
+class PinActivity: AppCompatActivity(), ShuffleNumberKeyboard.KeypadListener {
     private lateinit var binding: ActivityPinBinding
     private val viewModel: PinViewModel by viewModels()
 
@@ -15,5 +16,20 @@ class PinActivity: AppCompatActivity() {
         binding = ActivityPinBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        binding.shuffleKeyboard.setKeypadListener(this)
+    }
+
+    override fun onClickNum(num: String) {
+        viewModel.input(num)
+    }
+
+    override fun onClickDelete() {
+        viewModel.delete()
+    }
+
+    override fun onClickDone() {
+        viewModel.done()
     }
 }
